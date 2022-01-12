d1da3ac10d66b26a281a772fceea94b35c3c8d83
max-hagi <msavehilaghi99@gmail.com>
2022-01-11 22:11:20 -0500

    Updated music cog

--- music.py
+++ music.py
@@ -2,10 +2,7 @@ import discord
 from discord.ext import commands
 import youtube_dl
 
-
 class Music(commands.Cog):
-    queue = []
-
     def __init__(self, client):
         self.client = client
 
@@ -25,11 +22,20 @@ class Music(commands.Cog):
 
     @commands.command()
     async def play(self, ctx, url):
-        ctx.voice_client.stop()
-        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-                          'options': '-vn'}
+        if ctx.author.voice is None:
+            await ctx.send("You're not in a voice channel")
+        voice_channel = ctx.author.voice.channel
+        if ctx.voice_client is None:
+            await voice_channel.connect()
+        else:
+            await ctx.voice_client.move_to(voice_channel)
 
+
+        ctx.voice_client.stop()
+
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         YDL_OPTIONS = {'format': "bestaudio"}
+
         vc = ctx.voice_client
 
         with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
