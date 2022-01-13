43183756c0ac4f72265db14df5817effe2fdc0b1
Michael Ruiz <mruiz6@uwo.ca>
2022-01-12 22:49:12 -0500

    added img_search.py

---
+++ cogs/img_search.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import urllib
+import discord
+from discord.ext import commands
+
+class Image(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def picsearch(self, ctx, *, arg):
+        options = Options()
+        options.headless = True
+        service = Service(ChromeDriverManager().install())
+        browser = webdriver.Chrome(service=service, options=options)
+
+        browser.get('https://www.google.com/imghp?hl=en')
+        searchbar = browser.find_element(By.XPATH, '//*[@id="sbtc"]/div/div[2]/input')
+        searchbar.send_keys(arg)
+        searchbar.send_keys(Keys.RETURN)
+
+        image = browser.find_element(By.XPATH, '//*[@id="islrg"]/div[1]/div[1]/a[1]/div[1]/img').get_attribute('src')
+        urllib.request.urlretrieve(image, 'SEARCH_IMG.png')
+        browser.quit()
+
+        await ctx.send("Here is the first image that comes up:", file=discord.File('SEARCH_IMG.png'))
+
+def setup(client):
+    client.add_cog(Image(client))
